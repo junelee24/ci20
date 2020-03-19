c88f7fb845774bc8ffdbd6a18567f6c444ced4b3
June Lee <junel@Junes-MBP.home>
2020-03-19 16:10:26 -0400

    initial commit

--- lab/hw3/links.js
+++ lab/hw3/links.js
@@ -2,16 +2,19 @@ var pages= ["home", "writings", "drawings", "sketchbook", "prints", "travel", "o
 
 
 //select class
-// determine word length
-// attach links
 var words = document.querySelector(".sidebar");
-
+//for loop to create list
 for (let i = 0; i < pages.length; i++){
   console.log(pages[i]);
 
-
+//add links + jquery selector$ (pages[i]=the items in list)
+//links="(no content)"
   var links = `<a href = "" class="sidebar-link"> ${pages[i]}</a>`;
-
-
+// change the list into links using innerHTML
   words.innerHTML += links;
 }
+
+
+//reference:
+//https://www.w3schools.com/js/js_loop_for.asp
+//https://www.w3schools.com/jquery/jquery_ref_selectors.asp
